1823f611a0aaef4ecf7ab0f4d5942290df1254ce
Sixth <tech@withsix.co>
2024-03-27 03:20:15 +0100

    made chat pinger

--- ping.py
+++ ping.py
@@ -4,7 +4,7 @@ import time
 def ping_every_5_mins():
     while True:
         res = requests.get("https://chatgpt.withsix.co")
-        time.sleep(60)
+        time.sleep(120)
 
 if __name__ == "__main__":
     ping_every_5_mins()
